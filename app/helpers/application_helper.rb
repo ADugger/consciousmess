f1ace0036fefc9da359aa42895cd9aecb8a6e007
Alan Dugger <adugger1@my.westga.edu>
2020-02-29 01:36:15 -0500

    Additional page styling and view formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def sitelink(link)
+    if link.include?("http://")
+      puts link
+    elsif link.include?("https://")
+      puts link
+    else
+      link.insert(0, "http://")
+      link
+    end
+  end
 end
